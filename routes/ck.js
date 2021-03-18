ead591c4ab757276b8847dd0cdb696c3a51d7db0
Monate <angelina.schmidlin@hotmail.fr>
2021-03-18 01:29:13 +0100

    upload image

--- routes/ck.js
+++ routes/ck.js
@@ -1,19 +1,43 @@
-var express = require('express');
+const express = require('express');
 var router = express.Router();
-var bodyParser = require('body-parser');
-var multipart = require('connect-multiparty')
+const bodyparser = require ('body-parser');
+const multiparty = require('connect-multiparty');
+const { error } = require('console');
+
+const path = require('path');
+
+const fs = require('fs');
+
 var app = require('../app');
-var cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+const MuiltiPartyMiddleware = multiparty({uploadDir:"./uploads"});
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
-var multipartMiddleware = multipart({uploadDir: './uploads/images'});
-app.post('/ck/uploads', multipartMiddleware, function(req, resp) {
-  console.log(req.body, req.files);
-  // don't forget to delete all req.files when done
-});
+ app.use(express.static("uploads"));
+
+app.post('/ck/uploads', MuiltiPartyMiddleware, (req, res) =>{
+    
+    var TempFile = req.files.upload;
+    var TempPathfile = TempFile.path;
+
+   const targetPathUrl = path.join(__dirname,"../uploads/"+TempFile.name);
+
+   if(path.extname(TempFile.originalFilename).toLowerCase() === ".png" || ".jpg"){
+     
+    fs.rename(TempPathfile, targetPathUrl, err =>{
+
+        res.status(200).json({
+         uploaded: true,
+          url: `http://localhost:8080/${TempFile.originalFilename}`
+        });
+
+        if(err) return console.log(err);
+    })
+   }
+    //console.log(req.files);
+})
+
+
 
 module.exports = router;
